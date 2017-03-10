75ebc06bd8b3679952519bee3a692ea2a5e9f4a9
DSanchez <david38sanchez@gmail.com>
2017-03-10 10:05:59 -0500

    Add DataCollector files

---
+++ DataCollector/DwooDataCollector.php
@@ -0,0 +1,40 @@
+<?php
+namespace Dwoo\SymfonyBundle\DataCollector;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+
+/**
+ * Class DwooDataCollector
+ *
+ * @package Dwoo\SymfonyBundle\DataCollector
+ */
+class DwooDataCollector extends DataCollector
+{
+
+    /**
+     * Collects data for the given Request and Response.
+     *
+     * @param Request    $request   A Request instance
+     * @param Response   $response  A Response instance
+     * @param \Exception $exception An Exception instance
+     */
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        $this->data = [
+            'method'                   => $request->getMethod(),
+            'acceptable_content_types' => $request->getAcceptableContentTypes(),
+        ];
+    }
+
+    /**
+     * Returns the name of the collector.
+     *
+     * @return string The collector name
+     */
+    public function getName()
+    {
+        return 'dwoo';
+    }
+}
