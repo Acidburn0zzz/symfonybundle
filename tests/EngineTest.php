8dd2069c22a10bfa982136e3c2be1ff587e20583
DSanchez <david38sanchez@gmail.com>
2017-03-15 12:25:28 -0400

    Update tests files

--- tests/EngineTest.php
+++ tests/EngineTest.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Copyright (c) 2017
+ *
+ * @category  Library
+ * @package   Dwoo\SymfonBundle\tests
+ * @author    David Sanchez <david38sanchez@gmail.com>
+ * @copyright 2017 David Sanchez
+ * @license   http://dwoo.org/LICENSE LGPLv3
+ * @version   1.0.0
+ * @date      2017-03-14
+ * @link      http://symfony.dwoo.org/
+ */
+
 namespace Dwoo\SymfonyBundle\Tests;
 
 /**
